3f6e1023488e495e03f3174d1f0da6d0fb121eb4
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-02-19 21:45:28 +0530

    some hitntrial

--- tests/amazonSignUp.spec.ts
+++ tests/amazonSignUp.spec.ts
@@ -8,10 +8,17 @@ test('Open the Amazon website', async ({page}) => {
     await page.goto('https://www.amazon.com');
     await expect(page).toHaveURL('https://www.amazon.com');
     await page.getByRole('link', { name: 'Start here.' }).click();
+    await page.waitForLoadState('networkidle');
     await page.locator('#ap_customer_name').fill(process.env.AMAZON_NAME);
     await page.locator('#ap_email').fill(process.env.AMAZON_EMAIL);
     await page.locator('#ap_password').fill (process.env.AMAZON_PASSWORD);
     await page.locator('#ap_password_check').fill(process.env.AMAZON_PASSWORD);
-    await page.locator('#continue').click();
-    await expect(page).toHaveTitle('Verify email address');
+    await page.locator('#continue').click();;
+    if (Headers.name == "Solve this puzzle to protect your account") {
+        
+        
+    } else {
+        await expect(page).toHaveTitle('Verify email address');
+    }
+    
 });
